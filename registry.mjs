9e42dbeb63ddbf3755000ec64ad8537d1c5f53ac
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2024-02-21 11:05:30 +0200

    import from component.specs

--- registry.mjs
+++ registry.mjs
@@ -1,5 +1,6 @@
 import { Reference } from './lib/reference.mjs';
-export { GUID, Specs } from 'utils';
+export { Specs } from 'component.specs';
+export { GUID } from 'component.uuid';
 export { TypeReference } from './lib/type-ref.mjs';
 export { Reference };
 
