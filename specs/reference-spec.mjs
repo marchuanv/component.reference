c4ed1dbb4ed18c29254dc8ea80973f04862441ce
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2024-02-21 12:33:25 +0200

    refactored

--- specs/reference-spec.mjs
+++ specs/reference-spec.mjs
@@ -1,14 +1,35 @@
-import { GUID, Reference } from '../registry.mjs';
+import { Reference } from '../registry.mjs';
 import { Animal } from './index.mjs';
 describe('Reference Specifiction Test: ', () => {
-    describe(`when create a reference for the ${Animal.mane} class`, () => {
-        let animalRef = null;
+    describe(`when creating references for the ${Animal.name} class given the same namespace and reference names`, () => {
+        let animalRef1 = null;
+        let animalRef2 = null;
         beforeAll(() => {
-            animalRef = new Reference('animal', Animal);
+            animalRef1 = new Reference('animal', Animal, 'dog');
+            animalRef2 = new Reference('animal', Animal, 'dog');
         });
-        it(`should have equality when constructing references of the same ${Animal.name} class`, () => {
-            const animal1 = animalRef.construct(new GUID({ name: 'dog' }));
-            const animal2 = animalRef.construct(new GUID({ name: 'dog' }));
+        it('should have equality when constructing', () => {
+            const animal1 = animalRef1.construct();
+            const animal2 = animalRef2.construct();
+            expect(animal1).toBeDefined();
+            expect(animal1).not.toBeNull();
+            expect(animal1).toBeInstanceOf(Animal);
+            expect(animal2).toBeDefined();
+            expect(animal2).not.toBeNull();
+            expect(animal2).toBeInstanceOf(Animal);
+            expect(animal1).toBe(animal2);
+        });
+    });
+    describe(`when creating references for the ${Animal.name} class given the same namespace but different reference names`, () => {
+        let animalRef1 = null;
+        let animalRef2 = null;
+        beforeAll(() => {
+            animalRef1 = new Reference('animal', Animal, 'dog1');
+            animalRef2 = new Reference('animal', Animal, 'dog2');
+        });
+        it('should not have equality when constructing', () => {
+            const animal1 = animalRef1.construct();
+            const animal2 = animalRef2.construct();
 
             expect(animal1).toBeDefined();
             expect(animal1).not.toBeNull();
@@ -17,11 +38,19 @@ describe('Reference Specifiction Test: ', () => {
             expect(animal2).not.toBeNull();
             expect(animal2).toBeInstanceOf(Animal);
 
-            expect(animal1).toBe(animal2);
+            expect(animal1).not.toBe(animal2);
         });
-        it(`should not have equality when constructing references of the same ${Animal.name} class`, () => {
-            const animal1 = animalRef.construct(new GUID({ name: 'dog1' }));
-            const animal2 = animalRef.construct(new GUID({ name: 'dog2' }));
+    });
+    describe(`when creating references for the ${Animal.name} class given different namespaces but the same reference names`, () => {
+        let animalRef1 = null;
+        let animalRef2 = null;
+        beforeAll(() => {
+            animalRef1 = new Reference('animal.cat', Animal, 'dog');
+            animalRef2 = new Reference('animal.dog', Animal, 'dog');
+        });
+        it('should not have equality', () => {
+            const animal1 = animalRef1.construct();
+            const animal2 = animalRef2.construct();
 
             expect(animal1).toBeDefined();
             expect(animal1).not.toBeNull();
